73d1f07c70d4797c936d1af40e22e6e185564c69
wincher <wincher.hoo@gmail.com>
2021-03-15 17:51:17 +0800

    feat: add stage0 with  mvn archetype:generate -DgroupId=cn.wincher -DartifactId=stage0 -DarchetypeArtifactId=maven-archetype-quickstart

---
+++ stage0/src/main/java/cn/wincher/App.java
@@ -0,0 +1,13 @@
+package cn.wincher;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
