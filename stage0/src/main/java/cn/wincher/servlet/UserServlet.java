154fcf0143d6c73c337eaa62be96a03a1e01b9cc
wincher <wincher.hoo@gmail.com>
2021-03-19 11:16:49 +0800

    feat: support web

---
+++ stage0/src/main/java/cn/wincher/servlet/UserServlet.java
@@ -0,0 +1,21 @@
+package cn.wincher.servlet;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+/**
+ * @author wincher
+ * <p> cn.wincher.servlet <p>
+ */
+public class UserServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.write("Hello, contextPath is " + req.getContextPath() );
+    }
+}
