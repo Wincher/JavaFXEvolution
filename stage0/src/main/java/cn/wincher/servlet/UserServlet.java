b2f696300cdabbebc1d79a816ba5730f0913ba98
wincher <wincher.hoo@gmail.com>
2021-03-24 17:58:58 +0800

    update:

--- stage0/src/main/java/cn/wincher/servlet/UserServlet.java
+++ stage0/src/main/java/cn/wincher/servlet/UserServlet.java
@@ -1,11 +1,12 @@
 package cn.wincher.servlet;
 
 import javax.servlet.ServletException;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
+import java.nio.charset.StandardCharsets;
 
 /**
  * @author wincher
@@ -15,7 +16,11 @@ public class UserServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        PrintWriter writer = resp.getWriter();
-        writer.write("Hello, contextPath is " + req.getContextPath() );
+        //与OutputStream互斥,一次请求中不能都调用, 是对其的封装, 并且一个写的是byte流, 一个写的是字符串
+//        PrintWriter writer = resp.getWriter();
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.write(("Hello, contextPath is " + req.getContextPath()).getBytes(StandardCharsets.UTF_8) );
+        outputStream.flush();
+        outputStream.close();
     }
 }
